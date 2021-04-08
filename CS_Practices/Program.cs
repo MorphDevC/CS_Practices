7e3ab0eb9b45812ccc61c1b06387f9672e84faab
MorphDevC <morph@jelly.su>
2021-04-08 10:26:21 +0300

    StartPoint

---
+++ CS_Practices/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS_Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
